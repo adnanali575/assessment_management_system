c67a0885f3b6dd7e4053ff15116c25c59076eb07
adnanali575 <adnanali25753@gmail.com>
2023-10-10 12:15:35 -0700

    fixed some more UI bugs

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen w-screen bg-gray-50 space-y-5">
+      <div className="min-h-screen w-screen bg-gray-50 space-y-5 text-sm">
         <div className="flex flex-col items-center w-fit mx-auto gap-2 sm:gap-4 md:gap-8 sm:p-4">
           {assessmentsData.map((assessment, i) => (
             <AssessmentsTable key={i} assessment={assessment} />
--- src/components/AssessmentsTable.tsx
+++ src/components/AssessmentsTable.tsx
@@ -9,10 +9,10 @@ const AssessmentsTable: React.FC<any> = ({ assessment }) => {
         <table className="min-w-full border-collapse w-full">
           <thead>
             <tr>
-              <th className="border bg-blue-700 text-white font-medium text-left px-4 py-2">
+              <th className="border bg-blue-700 text-white font-medium text-left px-2 sm:px-4 py-2">
                 Subject
               </th>
-              <th className="border bg-blue-700 text-white font-medium text-left px-4 py-2">
+              <th className="border bg-blue-700 text-white font-medium text-left px-2 sm:px py-2">
                 Title
               </th>
               <th className="border bg-blue-700 text-white font-medium text-left px-4 py-2 hidden md:table-cell">
@@ -32,8 +32,8 @@ const AssessmentsTable: React.FC<any> = ({ assessment }) => {
                     : `hover:bg-blue-100 active:bg-blue-50 transition-all duration-200`
                   } border cursor-pointer`}
               >
-                <td className={`border px-4 py-2`}>{item.subject}</td>
-                <td className="border px-4 py-2 min-w-[200px]">
+                <td className={`border px-2 sm:px py-2`}>{item.subject}</td>
+                <td className="border px-2 sm:px py-2 sm:min-w-[200px]">
                   {item.title}
                 </td>
                 <td className="border px-4 py-2 hidden md:table-cell">
